07df25bd2f517cc58dc9d9643a87f33a8428f37f
Brainsoap <vjaszai@brainsum.com>
2019-03-14 18:28:08 +0100

    QAP-74 | Work in progress.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,83 +9,108 @@ import config from './config';
 const pubsub = new PubSub();
 config.set();
 
-document.getElementById('input-send').addEventListener('click', () => {
+(function inputCard() {
+  
   var errorCount = 0;
-  var inputs = [];
-
   const inputWebsite1 = document.getElementById('input-website-1');
   const inputWebsite2 = document.getElementById('input-website-2');
   const inputEmail = document.getElementById('input-email');
   const inputCheckboxPolicy = document.getElementById('input-checkbox-agree-policy');
   const inputCheckboxNews = document.getElementById('input-checkbox-agree-news');
+  const inputSend = document.getElementById('input-send');
 
-  if (!validator.isURL(inputWebsite1.value, { require_protocol: true })) { inputs.push({ error: true, id: 'input-website-1', message: 'Not a valid URL' }); } else { inputs.push({ error: false, id: 'input-website-1' }); }
-  if (!validator.isURL(inputWebsite2.value, { require_protocol: true })) { inputs.push({ error: true, id: 'input-website-2', message: 'Not a valid URL' }); } else { inputs.push({ error: false, id: 'input-website-2' }); }
-  if (!validator.isEmail(inputEmail.value)) { inputs.push({ error: true, id: 'input-email', message: 'Not a valid E-mail' }); } else { inputs.push({ error: false, id: 'input-email' }); }
-  if (!inputCheckboxPolicy.checked) { inputs.push({ error: true, id: 'input-checkbox-agree-policy', message: 'Must be checked' }); } else { inputs.push({ error: false, id: 'input-checkbox-agree-policy' }); }
 
-  // Form loading subscriptions
-  (() => {
-    const form1 = document.getElementById('form1');
-    const form2 = document.getElementById('form2');
+axios.interceptors.response.use(function (response) {
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Do something with response error
+  console.log(error);
+});
 
-    function showFormLoading() {
-      form1.classList.add('loading');
-    }
-
-    pubsub.subscribe('showFormLoading', showFormLoading);
-
-    function removeFormLoading() {
-      form1.classList.remove('loading');
-    }
-
-    pubsub.subscribe('removeFormLoading', removeFormLoading);
-
-    function showFormAfter() {
-      form1.classList.remove('loading');
-      form1.style = 'display: none;';
-      form2.style = 'display: initial;';
-    }
-
-    pubsub.subscribe('showFormAfter', showFormAfter);
-  })();
-
-  inputs.forEach((input) => {
-    if (input.error) {
-      errorCount += 1;
-      document.getElementById(input.id).style = 'border: 3px solid red';
-      document.querySelector(`#${input.id} ~ .error`).textContent = input.message;
-    } else {
-      document.getElementById(input.id).style = 'border: none';
-      document.querySelector(`#${input.id} ~ .error`).innerHTML = '';
+  // We input the first website URL
+  inputWebsite1.addEventListener('input', (e) => {
+    if (validator.isURL(e.target.value, { require_protocol: true })) {
+      console.log(e.target.value);
+      axios.get(e.target.value).catch((error) => {
+        console.log('Error: ', error.response);
+      });
     }
   });
 
-  // There was no validation erro, so we send data.
-  if (errorCount <= 0) {
-    pubsub.publish('showFormLoading');
+  // We click the Seend button
+  inputSend.addEventListener('click', () => {
 
-    axios.post(`${window.QAConfig.api.hosts.base.prod.protocol}${window.QAConfig.api.hosts.base.prod.domain}${window.QAConfig.api.hosts.base.prod.path}`, {
-      reference_url: inputWebsite1.value,
-      test_url: inputWebsite2.value,
-      newsletter: inputCheckboxNews.checked,
-      privacy_policy: inputCheckboxPolicy.checked,
-      email: inputEmail.value
-    }).then((response) => {
-      if (response.status >= 200 && response.status < 300) {
-        document.getElementById('output-website-1').textContent = inputWebsite1.value;
-        document.getElementById('output-website-2').textContent = inputWebsite2.value;
-        document.getElementById('output-email').textContent = inputEmail.value;
-        document.getElementById('output-email').href = `mailto:${inputEmail.value}`;
-
-        pubsub.publish('showFormAfter');
+    var inputs = [];
+  
+    if (!validator.isURL(inputWebsite1.value, { require_protocol: true })) { inputs.push({ error: true, id: 'input-website-1', message: 'Not a valid URL' }); } else { inputs.push({ error: false, id: 'input-website-1' }); }
+    if (!validator.isURL(inputWebsite2.value, { require_protocol: true })) { inputs.push({ error: true, id: 'input-website-2', message: 'Not a valid URL' }); } else { inputs.push({ error: false, id: 'input-website-2' }); }
+    if (!validator.isEmail(inputEmail.value)) { inputs.push({ error: true, id: 'input-email', message: 'Not a valid E-mail' }); } else { inputs.push({ error: false, id: 'input-email' }); }
+    if (!inputCheckboxPolicy.checked) { inputs.push({ error: true, id: 'input-checkbox-agree-policy', message: 'Must be checked' }); } else { inputs.push({ error: false, id: 'input-checkbox-agree-policy' }); }
+  
+    // Form loading subscriptions
+    (() => {
+      const form1 = document.getElementById('form1');
+      const form2 = document.getElementById('form2');
+  
+      function showFormLoading() {
+        form1.classList.add('loading');
       }
-    }).catch(() => {
-      pubsub.publish('removeFormLoading');
-      document.getElementById('output-error').textContent = 'There was an error while processing your request!';
+  
+      pubsub.subscribe('showFormLoading', showFormLoading);
+  
+      function removeFormLoading() {
+        form1.classList.remove('loading');
+      }
+  
+      pubsub.subscribe('removeFormLoading', removeFormLoading);
+  
+      function showFormAfter() {
+        form1.classList.remove('loading');
+        form1.style = 'display: none;';
+        form2.style = 'display: initial;';
+      }
+  
+      pubsub.subscribe('showFormAfter', showFormAfter);
+    })();
+  
+    inputs.forEach((input) => {
+      if (input.error) {
+        errorCount += 1;
+        document.getElementById(input.id).style = 'border: 3px solid red';
+        document.querySelector(`#${input.id} ~ .error`).textContent = input.message;
+      } else {
+        document.getElementById(input.id).style = 'border: none';
+        document.querySelector(`#${input.id} ~ .error`).innerHTML = '';
+      }
     });
-  }
-});
+
+    // There was no validation erro, so we send data.
+    if (errorCount <= 0) {
+      pubsub.publish('showFormLoading');
+  
+      axios.post(`${window.QAConfig.api.hosts.base.prod.protocol}${window.QAConfig.api.hosts.base.prod.domain}${window.QAConfig.api.hosts.base.prod.path}`, {
+        reference_url: inputWebsite1.value,
+        test_url: inputWebsite2.value,
+        newsletter: inputCheckboxNews.checked,
+        privacy_policy: inputCheckboxPolicy.checked,
+        email: inputEmail.value
+      }).then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          document.getElementById('output-website-1').textContent = inputWebsite1.value;
+          document.getElementById('output-website-2').textContent = inputWebsite2.value;
+          document.getElementById('output-email').textContent = inputEmail.value;
+          document.getElementById('output-email').href = `mailto:${inputEmail.value}`;
+  
+          pubsub.publish('showFormAfter');
+        }
+      }).catch(() => {
+        pubsub.publish('removeFormLoading');
+        document.getElementById('output-error').textContent = 'There was an error while processing your request!';
+      });
+    }
+  });
+})();
 
 // We create an other request. Crearing previous data.
 document.getElementById('input-again').addEventListener('click', () => {
